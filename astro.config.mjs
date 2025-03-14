c507e147560a3170519b60444a3b1451f88e50b8
Xavier SENENTE <xavier.senente@univ-fcomte.fr>
2025-03-14 11:51:28 +0100

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   experimental: { svg: true },
+  adapter: netlify(),
 });
